ad5a8d8a85aecfd06ea894ca3253bbbd8dce437b
chiara-pollicini <chiarap@kth.se>
2022-05-06 11:30:02 +0200

    created functions for GDI
    one for the control group and one for the subjects

---
+++ Functions/GDI_computation.m
@@ -0,0 +1,123 @@
+%% GDI computation %%
+% 1 subject at the time --> or should it be the full subject groups?
+
+function [] = GDI_computation(object, i, CG_Dataset)
+
+object.Subject(i).GDI = [];
+
+G_L = [];
+G_R = [];
+
+data = object.Subject(i).Angle;
+for i = 1:length(data.Sagittal.Pelvis.left(1,:))
+        % Pelvis
+    Lpel_tilt = data.Sagittal.Pelvis.left(:,i);
+    Lpel_obl = data.Frontal.Pelvis.left(:,i);
+    Lpel_rot = data.Transverse.Pelvis.left(:,i);
+        %Hip
+    Lhip_flex = data.Sagittal.Hip.left(:,i);
+    Lhip_abd = data.Frontal.Hip.left(:,i);
+    Lhip_rot = data.Transverse.Hip.left(:,i);
+        %Knee
+    Lknee_flex = data.Sagittal.Knee.left(:,i);
+        %Ankle
+    Lankle_dors = data.Sagittal.Ankle.left(:,i);
+        %Foot progression
+    Lfoot_prog = data.Transverse.Foot.left(:,i);
+    
+        % ceation of the matrix G
+    Lg = [Lpel_tilt; Lpel_obl; Lpel_rot; Lhip_flex; Lhip_abd; Lhip_rot; Lknee_flex; Lankle_dors; Lfoot_prog];
+    G_L = [G_L, Lg];
+end
+
+
+for i = 1:length(data.Sagittal.Pelvis.right(1,:))
+        % Pelvis
+    Rpel_tilt = data.Sagittal.Pelvis.right(:,i);
+    Rpel_obl = data.Frontal.Pelvis.right(:,i);
+    Rpel_rot = data.Transverse.Pelvis.right(:,i);
+        %Hip
+    Rhip_flex = data.Sagittal.Hip.right(:,i);
+    Rhip_abd = data.Frontal.Hip.right(:,i);
+    Rhip_rot = data.Transverse.Hip.right(:,i);
+        %Knee
+    Rknee_flex = data.Sagittal.Knee.right(:,i);
+        %Ankle
+    Rankle_dors = data.Sagittal.Ankle.right(:,i);
+        %Foot progression
+    Rfoot_prog = data.Transverse.Foot.right(:,i);
+    
+        % ceation of the matrix G       
+    Rg = [Rpel_tilt; Rpel_obl; Rpel_rot; Rhip_flex; Rhip_abd; Rhip_rot; Rknee_flex; Rankle_dors; Rfoot_prog];
+    G_R = [G_R, Rg];
+end
+
+nL = size(G_L,2); % number of left gait cycles anaysed for the subject
+nR = size(G_R,2); % number of right gait cycles anaysed for the subject
+
+%% Singular Value Decomposition
+% U is the singular vector matix that we need because it is mxm => matric G
+%is mxn with m=459 and in the article there are 459 f vectors
+% U is the f-basis (f) vectors indicating the gait features
+% But (f) should be unit length singular vector so its needed to normalize
+% all the vectors of the matrix (2-norm)
+
+[U_L,S_L,V_L]=svd(G_L);
+F_L = [];
+l = length(U_L);
+for j = 1:l
+    f = U_L(:,j)/(norm(U_L(:,j)));
+    F_L = [F_L, f];
+end
+
+[U_R,S_R,V_R]=svd(G_R);
+F_R = [];
+l = length(U_R);
+for j = 1:l
+    f = U_R(:,j)/(norm(U_R(:,j)));
+    F_R = [F_R, f];
+end
+
+%% Featrue components calculation
+m_crit = length(GDI_control.c);
+CL = zeros(m_crit, nL); %is the matix containing all the featur components column vectors, one for each controll gait cycle.
+for j = 1 : nL
+    g = G_L(:,j);
+    for k = 1:m_crit
+        f_k = F_L(:,k);
+        c_k = dot(g,f_k);
+        CL (k,j) = c_k;   %each colum refers to a gait cycle, "c" are colum vectros, one for each gait cycle
+    end
+end
+
+CR = zeros(m_crit, nR); %is the matix containing all the featur components column vectors, one for each controll gait cycle.
+for j = 1 : nR
+    g = G_R(:,j);
+    for k = 1:m_crit
+        f_k = F_R(:,k);
+        c_k = dot(g,f_k);
+        CR (k,j) = c_k;   %each colum refers to a gait cycle, "c" are colum vectros, one for each gait cycle
+    end
+end
+
+%% GDI calculation
+ 
+for j = 1:nL
+    GDI_i = log(norm(CL(:,j)- CG_Dataset.GDI_control.c));
+    zGDI_i = (GDI_i - CG_Dataset.GDI_control.mean)/CG_Dataset.GDI_control.sd;
+    GDI.left(j) = 100 - (10 * zGDI_i);
+end
+
+object.Subject(i).GDI.meanL = mean(GDI.left);
+object.Subject(i).GDI.sdL = std(GDI.left);
+
+for j = 1:nR
+    GDI_i = log(norm(CR(:,j)-CG_Dataset.GDI_control.c));
+    zGDI_i = (GDI_i - CG_Dataset.GDI_control.mean)/CG_Dataset.GDI_control.sd;
+    GDI.right(j) = 100 - (10 * zGDI_i);
+end
+
+object.Subject(i).GDI.meanR = mean(GDI(i).right);
+object.Subject(i).GDI.sdR = std(GDI(i).right);
+end
+
---
+++ Functions/GDI_control_calculation.m
@@ -0,0 +1,159 @@
+%% GDI - control group %%
+% calculation of the GDI parameters for the controll group.
+
+% INPUT:
+% CG_Dataset  :   dataset with the kinematic angles of the control group.
+%                  Created using the MoveAbilityLab.m structure.
+
+% OUTPUT:
+% c_control    :   Feature comonents colum vector, resuting from the mean
+%                  of the c vectros for each gaitcycle counted in the
+%                  control group.
+
+function [CG_Dataset] = GDI_control_calculation(CG_Dataset)
+
+%GC_Dataset is already composed by vectors with 100 elemets, we decide to
+%sample with 1% increment.
+
+%% matrix G creation
+G = [];
+data = CG_Dataset.Angle;
+for i = 1:length(data.Sagittal.Pelvis.left(1,:))
+        % Pelvis
+    Lpel_tilt = data.Sagittal.Pelvis.left(:,i);
+    Lpel_obl = data.Frontal.Pelvis.left(:,i);
+    Lpel_rot = data.Transverse.Pelvis.left(:,i);
+        %Hip
+    Lhip_flex = data.Sagittal.Hip.left(:,i);
+    Lhip_abd = data.Frontal.Hip.left(:,i);
+    Lhip_rot = data.Transverse.Hip.left(:,i);
+        %Knee
+    Lknee_flex = data.Sagittal.Knee.left(:,i);
+        %Ankle
+    Lankle_dors = data.Sagittal.Ankle.left(:,i);
+        %Foot progression
+    Lfoot_prog = data.Transverse.Foot.left(:,i);
+    
+        % ceation of the matrix G
+    Lg = [Lpel_tilt; Lpel_obl; Lpel_rot; Lhip_flex; Lhip_abd; Lhip_rot; Lknee_flex; Lankle_dors; Lfoot_prog];
+    G = [G, Lg];
+end
+
+
+for i = 1:length(data.Sagittal.Pelvis.right(1,:))
+        % Pelvis
+    Rpel_tilt = data.Sagittal.Pelvis.right(:,i);
+    Rpel_obl = data.Frontal.Pelvis.right(:,i);
+    Rpel_rot = data.Transverse.Pelvis.right(:,i);
+        %Hip
+    Rhip_flex = data.Sagittal.Hip.right(:,i);
+    Rhip_abd = data.Frontal.Hip.right(:,i);
+    Rhip_rot = data.Transverse.Hip.right(:,i);
+        %Knee
+    Rknee_flex = data.Sagittal.Knee.right(:,i);
+        %Ankle
+    Rankle_dors = data.Sagittal.Ankle.right(:,i);
+        %Foot progression
+    Rfoot_prog = data.Transverse.Foot.right(:,i);
+    
+        % ceation of the matrix G       
+    Rg = [Rpel_tilt; Rpel_obl; Rpel_rot; Rhip_flex; Rhip_abd; Rhip_rot; Rknee_flex; Rankle_dors; Rfoot_prog];
+    G = [G, Rg];
+end
+
+n = size(G,2); % number of gait cycles in the control group
+%% Singular Value Decomposition
+[U,S,V]=svd(G);
+% U is the singular vector matix that we need because it is mxm => matric G
+%is mxn with m=459 and in the article there are 459 f vectors
+% U is the f-basis (f) vectors indicating the gait features
+% But (f) should be unit length singular vector so its needed to normalize
+% all the vectors of the matrix (2-norm)
+
+F = [];
+l = length(U);
+for j = 1:l
+    f = U(:,j)/(norm(U(:,j)));
+    F = [F, f];
+end
+
+% F is the matrix containing column vectors f of unit length representing
+% the gait fearures.
+
+s = diag(S); %column vector with the singular values of the matrix G
+
+%% M_crit calculation
+m_max = length(s);
+
+%VAF calculation
+VAF = []; %vector containing all the results of VAF based on the no. of "m", each row is a diff m
+lambda = s.^2;
+lambda_tot = sum(lambda);
+for m = 1 : m_max
+    lambda_m = lambda(1:m);
+    VAF_m = 100*(sum(lambda_m)/lambda_tot);
+    VAF = [VAF; VAF_m];
+    %if VAF >= 98
+        %m_crit_vaf = m;
+        %break
+    %end
+end
+
+%phy calculation
+Phy = []; %matrix of the fithfulness of the reconstructed vector, each row is a different "m" while the colums identify the gait cycle used
+
+
+for i = 1 : n
+    g = G(:,i);
+    phy_g = [];
+    for m = 1:m_max
+        g_mk = [];
+        for k = 1:m
+            f_k = F(:,k);
+            c_k = dot(g,f_k);
+            g_mk = [g_mk, (c_k*f_k)]; %matrix containing the k "f" vectors moltiplied by the corresponding k "c" feature comonent. 
+                                      %Each column is a vector, thus to calculate g_m is needed the sum of the rows creating a column vector
+        end
+        g_m = sum(g_mk,2);
+        phy_m = dot(g,g_m)/(norm(g))^2;
+        phy_g = [phy_g; phy_m];
+    end
+    Phy = [Phy, phy_g];
+end
+
+phy_mean = mean(Phy,2); %is the mean among each gait cycle reconstructed by "m" features
+
+% selection of m_crit based on the article by M.H. Schwartz (2008)
+for m= 1:m_max
+    if VAF(m)>=98 && phy_mean(m)>=0.98
+        m_crit = m;
+        break
+    end
+end
+
+%% Feaure components c
+
+C = []; %is the matix containing all the featur components column vectors, one for each controll gait cycle.
+
+for i = 1 : n
+    g = G(:,i);
+    for k = 1:m_crit
+        f_k = F(:,k);
+        c_k = dot(g,f_k);
+        C(k,i) = c_k;
+    end
+end
+
+CG_Dataset.GDI_control.c = mean(C,2);
+
+%% Raw GDI for the control group
+ 
+for i = 1 : n
+    c_i = C(:,i); % colum vector for the i-th gait cycle in the control group
+    CG_Dataset.GDI_control.GDI(i) = log(norm(c_i - CG_Dataset.GDI_control.c));
+end
+CG_Dataset.GDI_control = [];
+CG_Dataset.GDI_control.mean = mean (CG_Dataset.GDI_control.GDI());
+CG_Dataset.GDI_control.sd = std(CG_Dataset.GDI_control.GDI());
+
+end
